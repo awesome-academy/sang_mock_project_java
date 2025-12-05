3577393184eff4d78431066f845e349de0430d31
sangct-1630 <cao.thanh.sang@sun-asterisk.com>
2025-12-05 14:52:41 +0700

    [95422] Admin auth

--- src/main/java/com/sun/ems/controller/admin/AuthController.java
+++ src/main/java/com/sun/ems/controller/admin/AuthController.java
@@ -1,5 +1,8 @@
 package com.sun.ems.controller.admin;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,15 @@ public class AuthController {
 
     @GetMapping("/login")
     public String loginPage() {
+    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null 
+			&& authentication.isAuthenticated() 
+			&& !(authentication instanceof AnonymousAuthenticationToken)
+		) {
+            return "redirect:/admin/dashboard";
+        }
+        
         return "admin/login";
     }
 }
